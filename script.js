690cff9fe35e628fb1a675850c50e2b1a9bc43e2
ryangholland <rghcode@gmail.com>
2024-03-09 13:25:27 -0500

    Refactor JS, add transition to dropdowns

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 const featuresButton = document.querySelector("[data-features-button]");
 const featuresDropdown = document.querySelector("[data-features-dropdown]");
 const featuresArrow = document.querySelector("[data-features-arrow]");
-
 const companyButton = document.querySelector("[data-company-button]");
 const companyDropdown = document.querySelector("[data-company-dropdown]");
 const companyArrow = document.querySelector("[data-company-arrow]");
 
-const toggleDropdown = (button, dropdown, arrow, arrowUp, arrowDown) => {
-  if (dropdown.hidden) {
-    dropdown.hidden = false;
-    arrow.src = arrowUp;
+const ARROWUP = "./images/icon-arrow-up.svg";
+const ARROWDOWN = "./images/icon-arrow-down.svg";
+
+const toggleDropdown = (dropdown, arrow) => {
+  if (!dropdown.classList.contains("active")) {
+    dropdown.classList.add("active");
+    arrow.src = ARROWUP;
   } else {
-    dropdown.hidden = true;
-    arrow.src = arrowDown;
+    closeDropdown(dropdown, arrow);
   }
+};
+
+const closeDropdown = (dropdown, arrow) => {
+  dropdown.classList.remove("active");
+  arrow.src = ARROWDOWN;
 };
 
 document.addEventListener("click", (e) => {
   if (e.target === featuresButton) {
-    toggleDropdown(featuresButton, featuresDropdown, featuresArrow, "./images/icon-arrow-up.svg", "./images/icon-arrow-down.svg");
-    companyDropdown.hidden = true;
-    companyArrow.src = "./images/icon-arrow-down.svg";
+    toggleDropdown(featuresDropdown, featuresArrow);
+    closeDropdown(companyDropdown, companyArrow);
   } else if (e.target === companyButton) {
-    toggleDropdown(companyButton, companyDropdown, companyArrow, "./images/icon-arrow-up.svg", "./images/icon-arrow-down.svg");
-    featuresDropdown.hidden = true;
-    featuresArrow.src = "./images/icon-arrow-down.svg";
+    toggleDropdown(companyDropdown, companyArrow);
+    closeDropdown(featuresDropdown, featuresArrow);
   } else if (e.target !== featuresDropdown && e.target !== companyDropdown) {
-    featuresDropdown.hidden = true;
-    featuresArrow.src = "./images/icon-arrow-down.svg";
-    companyDropdown.hidden = true;
-    companyArrow.src = "./images/icon-arrow-down.svg";
+    closeDropdown(companyDropdown, companyArrow);
+    closeDropdown(featuresDropdown, featuresArrow);
   }
 });
